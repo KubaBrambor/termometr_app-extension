07968e68622a6e6e935ec0e572876e5e8909c77a
Jakub Brambor <kuba.brambor@gmail.com>
2020-06-10 16:45:53 +0200

    deleted Gadowianka since it wasnt updated and added few new locations to app, fixed bug with shaking window after location change

--- termometr/src/app/card/card.component.ts
+++ termometr/src/app/card/card.component.ts
@@ -36,21 +36,21 @@ export class CardComponent implements OnInit {
         console.warn('Server response code: ' + data.status)
         for(let i=0; i<data.body.result.records.length; i++){
           this.localArr.push(data.body.result.records[i])
-          console.log(this.localArr[i]) 
+          console.log(this.localArr[i])
           if(this.localArr[i]._id == localStorage.getItem(this.indexDataStorage)){
             this.indexFound = true;
           }
         }
-        // /* Delete record from Widawska street, since it wasn't updated for long time */
-        // this.localArr.splice(0,1);
-        // console.log(this.localArr)
+        /* Delete record from Widawska and Gądowianka street, since it wasn't updated for long time */
+        this.localArr.splice(0,2);
+        console.log(this.localArr)
 
         if(this.indexFound){
           this.buttonDisable = true;
 
           this.temperature = this.localArr[localStorage.getItem(this.indexDataStorage)].T_Powietrza;
           console.log(this.temperature);
-      
+
           this.localisation = this.localArr[localStorage.getItem(this.indexDataStorage)].Lokalizacja_Opis;
           console.log(this.localisation);
 
@@ -71,22 +71,22 @@ export class CardComponent implements OnInit {
         } else {
           this.temperature = this.localArr[0].T_Powietrza;
           console.log(this.temperature);
-        
+
           this.localisation = this.localArr[0].Lokalizacja_Opis;
           console.log(this.localisation);
-  
+
           this.rain = this.localArr[0].Opad_Typ;
           console.log(this.rain);
-  
+
           this.terrainTemperature = this.localArr[0].T_Grunt;
           console.log(this.terrainTemperature);
-  
+
           this.wetness = this.localArr[0].Wilgotnosc;
           console.log(this.wetness);
-  
+
           this.time = this.localArr[0].Czas_Rejestracji;
           console.log(this.time);
-  
+
           this.windDirection = this.localArr[0].Wiatr_Kierunek;
           console.log(this.windDirection);
 
@@ -105,24 +105,24 @@ export class CardComponent implements OnInit {
     this.http.getValues().subscribe((data:any) => {
       if(data.status === 200){
 
-        console.log('Server response code :' + data.status)
-        /* Clear localArr array for fresh weather data */
-        this.localArr.splice(0, this.localArr.length);
+        // console.log('Server response code :' + data.status)
+        // /* Clear localArr array for fresh weather data */
+        // this.localArr.splice(0, this.localArr.length);
 
-        for(let i=0; i<data.body.result.records.length; i++){
-          this.localArr.push(data.body.result.records[i]) 
-        }
+        // for(let i=0; i<data.body.result.records.length; i++){
+        //   this.localArr.push(data.body.result.records[i])
+        // }
 
-        /* Delete record from Widawska street, since it wasn't updated for long time */
-        this.localArr.splice(0,1)
-        console.log(this.localArr);
-      
+        // /* Delete record from Widawska street, since it wasn't updated for long time */
+        // this.localArr.splice(0,1)
+        // console.log(this.localArr);
+
         this.indexData = event.target.selectedIndex - 1;
         console.warn(this.indexData);
 
         this.temperature = this.localArr[this.indexData].T_Powietrza;
         console.log(this.temperature);
-    
+
         this.localisation = this.localArr[this.indexData].Lokalizacja_Opis;
         console.log(this.localisation);
 
@@ -140,7 +140,7 @@ export class CardComponent implements OnInit {
 
         this.windDirection = this.localArr[this.indexData].Wiatr_Kierunek;
         console.log(this.windDirection);
-                
+
         /* set button on, as default option, before checking with localisation */
         this.buttonDisable = false;
 
